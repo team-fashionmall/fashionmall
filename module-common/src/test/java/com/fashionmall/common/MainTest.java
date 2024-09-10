598a247c18618536ae8f09ba4adc63c0f6a57123
evan-song2 <mj.song@pet-friends.co.kr>
2024-09-10 20:12:55 +0900

    init: 기본 디렉토리 생성

---
+++ module-common/src/test/java/com/fashionmall/common/MainTest.java
@@ -0,0 +1,12 @@
+package com.fashionmall.common;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
---
+++ module-order-service/src/test/java/com/fashionmall/order/OrderApplicationTest.java
@@ -0,0 +1,11 @@
+package com.fashionmall.order;
+
+import org.junit.jupiter.api.Test;
+
+class OrderApplicationTest {
+
+    @Test
+    void test() {
+
+    }
+}
