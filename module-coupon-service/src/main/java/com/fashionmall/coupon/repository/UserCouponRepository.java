c57a26381366273a4bf4e4dbff97146262881c1a
noah <qkrwldn0818@naver.com>
2024-09-13 17:39:48 +0900

    feat : UserCouponRepository 생성

---
+++ module-coupon-service/src/main/java/com/fashionmall/coupon/repository/UserCouponRepository.java
@@ -0,0 +1,9 @@
+package com.fashionmall.coupon.repository;
+
+import com.fashionmall.coupon.entity.UserCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+
+    boolean existsByUserIdAndCouponId(Long userId, Long couponId);
+}
