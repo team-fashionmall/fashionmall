cddd9afaabc10659e13a45be42689aea432b6678
noah <qkrwldn0818@naver.com>
2024-09-13 13:53:16 +0900

    refactor: 매겨변수 추가, 매서드 수정 - totalCount 전체 리스트 개수 - totalPages 계산식 변경

--- module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
+++ module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
@@ -14,14 +14,14 @@ public class PageInfoResponseDto<T> {
     private List<T> content;
     private PageResponseDto pageInfo;
 
-    public static <T> PageInfoResponseDto<T> of(int page, int size, List<T> content) {
+    public static <T> PageInfoResponseDto<T> of(int page, int size, List<T> content, int totalCount) {
         int numberOfElements = Math.min(content.size(), size);
-        int totalPages = (int) Math.ceil((double) content.size() / size);
+        int totalPages = (int) Math.ceil((double) totalCount / size);
         PageResponseDto pageResponseDto = new PageResponseDto(
                 page,
                 size,
                 numberOfElements,
-                content.size(),
+                totalCount,
                 totalPages
         );
         List<T> subList = content.subList(0, numberOfElements);
