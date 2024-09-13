003e1bd1f283b4f8a9068dceec9de0796a6f3b86
noah <qkrwldn0818@naver.com>
2024-09-13 15:44:04 +0900

    refactor: 변수명 수정, 통일

--- module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
+++ module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
@@ -14,15 +14,15 @@ public class PageInfoResponseDto<T> {
     private List<T> content;
     private PageResponseDto pageInfo;
 
-    public static <T> PageInfoResponseDto<T> of(int page, int size, List<T> content, int totalCount) {
+    public static <T> PageInfoResponseDto<T> of(int pageNo, int size, List<T> content, int totalCount) {
         int numberOfElements = Math.min(content.size(), size);
-        int totalPages = (int) Math.ceil((double) totalCount / size);
+        int totalPage = (int) Math.ceil((double) totalCount / size);
         PageResponseDto pageResponseDto = new PageResponseDto(
-                page,
+                pageNo,
                 size,
                 numberOfElements,
                 totalCount,
-                totalPages
+                totalPage
         );
         List<T> subList = content.subList(0, numberOfElements);
         return new PageInfoResponseDto<>(subList, pageResponseDto);
--- module-common/src/main/java/com/fashionmall/common/response/PageResponseDto.java
+++ module-common/src/main/java/com/fashionmall/common/response/PageResponseDto.java
@@ -12,7 +12,7 @@ public class PageResponseDto {
     private int page; //페이지
     private int size; //화면에 표시할 콘텐츠 개수 제한
     private int numberOfElements; //화면에 표시된 콘텐츠 개수
-    private int totalContent; //전체 콘텐츠 개수
+    private int totalCount; //전체 콘텐츠 개수
     private int totalPage; //전체 페이지 수
 
 }
