c1563d6bfccbe6ecb5aa53a2f5e538facd7f5954
noah <qkrwldn0818@naver.com>
2024-09-13 15:50:19 +0900

    refactor: 페이징 정보 추가 - 다음 페이지 여부

--- module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
+++ module-common/src/main/java/com/fashionmall/common/response/PageInfoResponseDto.java
@@ -17,12 +17,14 @@ public class PageInfoResponseDto<T> {
     public static <T> PageInfoResponseDto<T> of(int pageNo, int size, List<T> content, int totalCount) {
         int numberOfElements = Math.min(content.size(), size);
         int totalPage = (int) Math.ceil((double) totalCount / size);
+        boolean hasNext = pageNo < totalPage;
         PageResponseDto pageResponseDto = new PageResponseDto(
                 pageNo,
                 size,
                 numberOfElements,
                 totalCount,
-                totalPage
+                totalPage,
+                hasNext
         );
         List<T> subList = content.subList(0, numberOfElements);
         return new PageInfoResponseDto<>(subList, pageResponseDto);
--- module-common/src/main/java/com/fashionmall/common/response/PageResponseDto.java
+++ module-common/src/main/java/com/fashionmall/common/response/PageResponseDto.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PageResponseDto {
 
-    private int page; //페이지
+    private int pageNo; //페이지
     private int size; //화면에 표시할 콘텐츠 개수 제한
     private int numberOfElements; //화면에 표시된 콘텐츠 개수
     private int totalCount; //전체 콘텐츠 개수
     private int totalPage; //전체 페이지 수
+    private boolean hasNext; //다음 페이지 여부
 
 }
