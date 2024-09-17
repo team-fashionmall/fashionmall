ed75181507a6f20bd1ada03653738eb88337acc8
noah <qkrwldn0818@naver.com>
2024-09-17 17:38:22 +0900

    refactor : 관리자 API 분리

--- module-coupon-service/src/main/java/com/fashionmall/coupon/controller/CouponQueryController.java
+++ module-coupon-service/src/main/java/com/fashionmall/coupon/controller/CouponQueryController.java
@@ -3,47 +3,21 @@ package com.fashionmall.coupon.controller;
 import com.fashionmall.common.response.CommonResponse;
 import com.fashionmall.common.response.PageInfoResponseDto;
 import com.fashionmall.common.util.ApiResponseUtil;
-import com.fashionmall.coupon.dto.request.CouponRequestDto;
-import com.fashionmall.coupon.dto.request.CouponUpdateRequestDto;
-import com.fashionmall.coupon.dto.response.CouponResponseDto;
+import com.fashionmall.coupon.dto.response.UserCouponResponseDto;
 import com.fashionmall.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CouponQueryController {
 
     private final CouponService couponService;
-
-    //(관리자)쿠폰조회
-    @GetMapping("/admin/coupon")
-    public CommonResponse<PageInfoResponseDto<CouponResponseDto>> getCouponList(@RequestParam(defaultValue = "1") int page) {
-        //관리자 인증 관련 추가
-        return ApiResponseUtil.success(couponService.getCoupons(page));
-    }
-
-    //(관리자)쿠폰등록
-    @PostMapping("/admin/coupon")
-    public CommonResponse<Long> publishCoupon(@RequestBody CouponRequestDto couponRequestDto) {
-        //관리자 인증 관련 추가
-        return ApiResponseUtil.success(couponService.publishCoupon(couponRequestDto));
-    }
-
-    //(관리자)쿠폰수정
-    @PutMapping("/admin/coupon/{couponId}")
-    public CommonResponse<Long> updateCoupon(@PathVariable("couponId") Long couponId,
-                                             @RequestBody CouponUpdateRequestDto couponUpdateRequestDto) {
-        //관리자 인증 관련 추가
-        return ApiResponseUtil.success(couponService.updateCoupon(couponId, couponUpdateRequestDto));
-    }
-
-    //(관리자)쿠폰삭제
-    @DeleteMapping("/admin/coupon/{couponId}")
-    public CommonResponse<Long> deleteCoupon(@PathVariable("couponId") Long couponId) {
-        //관리자 인증 관련 추가
-        return ApiResponseUtil.success(couponService.deleteCoupon(couponId));
-    }
 
     //쿠폰목록조회
     @GetMapping("/coupon")
