1292c69bb4d2be81a22aa0f9d3da64167155b7d4
noah <qkrwldn0818@naver.com>
2024-09-19 18:13:51 +0900

    feat : 주문 서비스 엔티티 생성

---
+++ module-order-service/src/main/java/com/fashionmall/order/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.fashionmall.order.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at", updatable = false)
+    protected LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ module-order-service/src/main/java/com/fashionmall/order/entity/BillingKey.java
@@ -0,0 +1,35 @@
+package com.fashionmall.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "billing_key")
+public class BillingKey extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "billing_key_id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "customer_uid")
+    private String customerUid; //구매자의 결제 수단 식별 고유번호
+
+    @Column(name = "card_nickname")
+    private String cardNickname; //카드 별명
+
+    @Column(name = "card_name")
+    private String cardName; //카드명
+
+    @Column(name = "card_number")
+    private String cardNumber; //마스킹 카드번호
+
+    @Column(name = "card_type")
+    private String cardType; //카드유형
+}
