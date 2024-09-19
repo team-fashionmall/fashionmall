8e732b7baaf432d6146bcf829f6d1031fc794f9e
minnieming <eunmilee9109.1@gmail.com>
2024-09-19 22:29:00 +0900

    feat : JWT Util (쿠키 관련 포함)

--- module-common/src/main/java/com/fashionmall/common/jwt/JwtUtil.java
+++ module-common/src/main/java/com/fashionmall/common/jwt/JwtUtil.java
@@ -1,12 +1,17 @@
 package com.fashionmall.common.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.fashionmall.common.exception.CustomException;
+import com.fashionmall.common.exception.ErrorResponseCode;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Base64;
@@ -69,7 +74,63 @@ public class JwtUtil {
                 .compact();
     }
 
+    // Access Token 쿠키에 넣어 생성
+    public ResponseCookie createAccessTokenCookie (String accessToken) { // accessToken 들어오는지 확인
+
+        return ResponseCookie.from("access_token", accessToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(ACCESS_TOKEN_EXPIRATION_TIME / 1000)
+                .sameSite("Strict")
+                .build();
+    }
+
+    // Refresh Token 쿠키에 넣어 생성
+    public ResponseCookie createRefreshTokenCookie (String refreshToken) { // refreshToken 들어오는지 확인
+
+        return ResponseCookie.from("access_token", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(REFRESH_TOKEN_EXPIRATION_TIME / 1000)
+                .sameSite("Strict")
+                .build();
+    }
+
+    // 쿠키 Response 하기
+    public void addCookiesToResponse (HttpServletResponse response, String accessToken, String refreshToken) { // token 들어오는지 확인
+
+        ResponseCookie accessCookie = createAccessTokenCookie(accessToken);
+        ResponseCookie refreshCookie = createRefreshTokenCookie(refreshToken);
+
+        response.addHeader("Set-AccessCookie", accessCookie.toString());
+        response.addHeader("Set-RefreshCookie", refreshCookie.toString());
+    }
+
     // JWT Token substring
-    //public String substringToken (String )
+    public String substringToken (String tokenValue) { // // tokenValue 들어오는지 확인
+        if (StringUtils.hasText (tokenValue) && tokenValue.startsWith (BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        throw new CustomException(ErrorResponseCode.JWT_NOT_FOUND_TOKEN);
+    }
+
+    // JWT Token 검증
+    public boolean validateToken (String token) { // token 들어오는지 확인
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
 
 }
