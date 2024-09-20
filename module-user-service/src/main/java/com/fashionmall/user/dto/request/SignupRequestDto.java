9125cb5256b4274c8280a1d3b90b9ac6169b1e14
minnieming <eunmilee9109.1@gmail.com>
2024-09-21 00:29:58 +0900

    feat : 회원가입 RequestDto

---
+++ module-user-service/src/main/java/com/fashionmall/user/dto/request/SignupRequestDto.java
@@ -0,0 +1,37 @@
+package com.fashionmall.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+// @Builder
+@NoArgsConstructor
+public class SignupRequestDto {
+
+    @NotBlank (message = "이메일을 입력해주세요")
+    @Email (message = "이메일 형식으로 입력해주세요")
+    private String email;
+
+    @NotBlank (message = "비밀번호를 입력해주세요")
+    @Pattern (regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\W)[a-zA-Z0-9\\W]{8,}$",
+            message = "대문자, 소문자, 특수문자, 숫자 포함 8자리 이상")
+    private String password;
+
+    @NotBlank (message = "이름을 입력해주세요")
+    @Pattern (regexp = "^[a-zA-Z0-9가-힣]{1,15}$", message = "특수문자 제외, 15자 이내")
+    private String username;
+
+    @NotBlank (message = "닉네임을 입력해주세요")
+    @Pattern (regexp = "^[a-zA-Z0-9가-힣]{6,}$", message = "특수문자 제외, 6자 이상")
+    private String nickname;
+
+    @NotBlank (message = "연락처를 입력해주세요")
+    @Pattern (regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다")
+    private String contact;
+
+    private boolean isAdmin = false; // User = false
+    // 여기 추가로 확인하기
+}
