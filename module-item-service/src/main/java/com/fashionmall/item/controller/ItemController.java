eaa1434820dbe4d00624fa3ceef838877ca550ad
minnieming <eunmilee9109.1@gmail.com>
2024-09-25 14:31:38 +0900

    feat : ItemController

---
+++ module-item-service/src/main/java/com/fashionmall/item/controller/ItemController.java
@@ -0,0 +1,26 @@
+package com.fashionmall.item.controller;
+
+import com.fashionmall.common.response.CommonResponse;
+import com.fashionmall.common.util.ApiResponseUtil;
+import com.fashionmall.item.dto.request.ItemRequestDto;
+import com.fashionmall.item.dto.response.ItemResponseDto;
+import com.fashionmall.item.service.ItemService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j (topic = "item controller")
+@RestController
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping ("/admin/item")
+    public CommonResponse <ItemResponseDto> createItem (@Valid @RequestBody ItemRequestDto itemRequestDto) {
+        return ApiResponseUtil.success(itemService.createItem(itemRequestDto));
+    }
+}
