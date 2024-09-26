7576e4970e9db622bf88178b24d054213f0bb830
evan-song2 <mj.song@pet-friends.co.kr>
2024-09-26 11:43:01 +0900

    init: review 모듈 생성

---
+++ module-review-service/src/main/java/com/fashionmall/review/controller/ReviewQueryController.java
@@ -0,0 +1,13 @@
+package com.fashionmall.review.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewQueryController {
+
+    @GetMapping("/")
+    public void test() {
+
+    }
+}
