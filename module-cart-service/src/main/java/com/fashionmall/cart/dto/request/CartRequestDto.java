7364f171cc7ecd6315efec7deec0c90b45eb6539
minnieming <eunmilee9109.1@gmail.com>
2024-09-27 19:03:15 +0900

    feat : 장바구니 등록 request

---
+++ module-cart-service/src/main/java/com/fashionmall/cart/dto/request/CartRequestDto.java
@@ -0,0 +1,29 @@
+package com.fashionmall.cart.dto.request;
+
+import com.fashionmall.cart.entity.CartStatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartRequestDto {
+
+    @NotBlank (message = "상품아이디를 입력해주세요")
+    private Long itemId;
+
+    @Positive (message = "해당 상품의 개수를 입력해주세요")
+    private int cartQuantity;
+
+    @NotBlank(message = "상품 선택 여부")
+    private CartStatusEnum cartStatus;
+
+    @Positive (message = "상품의 colorId를 입력해주세요")
+    private String itemColor;
+
+    @Positive(message = "상품의 sizeId를 입력해주세요")
+    private String itemSize;
+}
