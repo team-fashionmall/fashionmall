b15595b08f70d6df110db0f6fdc1d93a11c584e2
minnieming <eunmilee9109.1@gmail.com>
2024-09-27 19:05:02 +0900

    feat : 장바구니 등록 controller

---
+++ module-cart-service/src/main/java/com/fashionmall/cart/controller/CartController.java
@@ -0,0 +1,22 @@
+package com.fashionmall.cart.controller;
+
+import com.fashionmall.cart.dto.request.CartRequestDto;
+import com.fashionmall.cart.service.CartService;
+import com.fashionmall.common.response.CommonResponse;
+import com.fashionmall.common.util.ApiResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j(topic = "cart controller")
+@RestController
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+    @PostMapping("/cart")
+    public CommonResponse<String> createCart (@RequestBody CartRequestDto cartRequestDto) {
+        return ApiResponseUtil.success(cartService.createCart(cartRequestDto));
+    }
+}
