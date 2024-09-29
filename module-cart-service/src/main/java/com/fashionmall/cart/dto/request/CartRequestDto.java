6b98762537bab87d43fa19a9e2d77b882e60c08a
minnieming <eunmilee9109.1@gmail.com>
2024-09-30 02:28:52 +0900

    refactor : RequestDto 변수 수정

--- module-cart-service/src/main/java/com/fashionmall/cart/dto/request/CartRequestDto.java
+++ module-cart-service/src/main/java/com/fashionmall/cart/dto/request/CartRequestDto.java
@@ -13,22 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CartRequestDto {
 
-
     @NotNull (message = "상품아이디를 입력해주세요")
     private Long itemId;
 
     @NotNull @Positive (message = "해당 상품의 개수를 입력해주세요")
     private int cartQuantity;
 
-    @NotNull
-    private CartStatusEnum cartStatus;
-
-    @NotNull @Positive (message = "상품의 colorId를 입력해주세요")
-    private String itemColor;
-
-    @NotNull @Positive(message = "상품의 sizeId를 입력해주세요")
-    private String itemSize;
-
-    private String itemName; // 추후 상품에서 받아올 예정
-    private int itemPrice;
 }
