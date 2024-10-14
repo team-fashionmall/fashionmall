5e21f3f764303853531e553ee577acf54b0a6f24
minnieming <eunmilee9109.1@gmail.com>
2024-10-14 13:28:53 +0900

    feat : pull 받아온것 init

--- module-cart-service/src/main/java/com/fashionmall/cart/controller/CartController.java
+++ module-cart-service/src/main/java/com/fashionmall/cart/controller/CartController.java
@@ -1,21 +1,23 @@
 package com.fashionmall.cart.controller;
 
 import com.fashionmall.cart.dto.request.CartRequestDto;
+import com.fashionmall.cart.dto.request.CartUpdateRequestDto;
+import com.fashionmall.cart.dto.response.CartUpdateResponseDto;
 import com.fashionmall.cart.service.CartService;
 import com.fashionmall.common.response.CommonResponse;
 import com.fashionmall.common.util.ApiResponseUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j(topic = "cart controller")
 @RestController
 @RequiredArgsConstructor
 public class CartController {
+
     private final CartService cartService;
+
     @PostMapping("/cart")
     public CommonResponse<String> createCart (@Valid @RequestBody CartRequestDto cartRequestDto) {
         Long userId = 1L;
