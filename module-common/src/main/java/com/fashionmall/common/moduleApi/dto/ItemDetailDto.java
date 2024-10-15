d56b17ebaa48f3c220d50af1a82c26c24b09eea6
noah <qkrwldn0818@naver.com>
2024-10-15 15:48:10 +0900

    feat : API 통신 Dto

---
+++ module-common/src/main/java/com/fashionmall/common/moduleApi/dto/ItemDetailDto.java
@@ -0,0 +1,16 @@
+package com.fashionmall.common.moduleApi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDetailDto {
+
+    private Long id;
+    private String name;
+    private int price;
+    private int quantity;
+}
