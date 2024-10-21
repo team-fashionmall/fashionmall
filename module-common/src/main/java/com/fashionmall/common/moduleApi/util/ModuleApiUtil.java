836294a6b248d5efb02e0b8af76787762cd6f113
minnieming <eunmilee9109.1@gmail.com>
2024-10-21 16:09:42 +0900

    feat : image 등록 gateway

--- module-common/src/main/java/com/fashionmall/common/moduleApi/util/ModuleApiUtil.java
+++ module-common/src/main/java/com/fashionmall/common/moduleApi/util/ModuleApiUtil.java
@@ -1,5 +1,7 @@
 package com.fashionmall.common.moduleApi.util;
 
+import com.fashionmall.common.moduleApi.dto.ImageDataDto;
+import com.fashionmall.common.moduleApi.dto.ImageUploadDto;
 import com.fashionmall.common.moduleApi.dto.OrderItemDto;
 import com.fashionmall.common.moduleApi.dto.ItemDetailDto;
 import com.fashionmall.common.moduleApi.dto.ItemDetailResponseDto;
@@ -10,6 +12,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -91,9 +94,21 @@ public class ModuleApiUtil {
                 orderItemDto,
                 new ParameterizedTypeReference<Void>() {},
                 headers());
+    private final String imageApi = "http://localhost:8000/api/image";
+
+    public Map <Long, String> uploadImageApi(List<ImageUploadDto> imageUploadDto) {
+        CommonResponse<Map<Long, String>> uploadImageApi = webClientUtil.post(
+                imageApi + "/uploadImageApi",
+                imageUploadDto,
+                new ParameterizedTypeReference<CommonResponse<Map<Long,String>>>() {}, // 응답 타입 명시
+                headers()
+        );
+
+        return uploadImageApi.getData();
     }
 
     private Map<String, String> headers (){
         return Map.of (HttpHeaders.CONTENT_TYPE, "application/json");
     }
+
 }
