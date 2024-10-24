fa9e6427d8e2783aefada5b21026c37a856118ea
minnieming <eunmilee9109.1@gmail.com>
2024-10-24 11:41:43 +0900

    refactor : Import 정리

--- module-image-service/src/main/java/com/fashionmall/image/service/ImageService.java
+++ module-image-service/src/main/java/com/fashionmall/image/service/ImageService.java
@@ -3,11 +3,6 @@ package com.fashionmall.image.service;
 
 import com.fashionmall.common.moduleApi.dto.ImageDataDto;
 import com.fashionmall.common.moduleApi.dto.ImageUploadDto;
-import com.fashionmall.common.response.PageInfoResponseDto;
-import com.fashionmall.image.dto.request.ImageUploadRequestDto;
-import com.fashionmall.image.dto.response.ImageListResponseDto;
-import com.fashionmall.image.dto.response.ImageUploadResponseDto;
-
 
 import java.util.List;
 import java.util.Map;
