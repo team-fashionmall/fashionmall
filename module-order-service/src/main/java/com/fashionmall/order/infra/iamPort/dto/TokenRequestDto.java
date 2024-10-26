3cb241f883f638966cb7db84ce9b8abefb421105
noah <qkrwldn0818@naver.com>
2024-10-27 00:09:02 +0900

    feat : order Api 재등록2

---
+++ module-order-service/src/main/java/com/fashionmall/order/infra/iamPort/dto/TokenRequestDto.java
@@ -0,0 +1,20 @@
+package com.fashionmall.order.infra.iamPort.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequestDto {
+
+    @JsonProperty("imp_key")
+    private String impKey;
+    @JsonProperty("imp_secret")
+    private String impSecret;
+
+}
---
+++ module-order-service/src/main/java/com/fashionmall/order/infra/iamPort/dto/TokenResponseDto.java
@@ -0,0 +1,17 @@
+package com.fashionmall.order.infra.iamPort.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenResponseDto {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("expired_at")
+    private int expiredAt;
+    @JsonProperty("now")
+    private int now;
+}
