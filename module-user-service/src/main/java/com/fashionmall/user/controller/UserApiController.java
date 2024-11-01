04afcffa495addb204483058b691d2119c95d7be
minnieming <eunmilee9109.1@gmail.com>
2024-11-02 00:46:42 +0900

    refactor : User 유저 인증 모듈 설정

--- module-user-service/src/main/java/com/fashionmall/user/controller/UserApiController.java
+++ module-user-service/src/main/java/com/fashionmall/user/controller/UserApiController.java
@@ -19,9 +19,9 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @GetMapping ("/confirm/{userName}")
-    public CommonResponse<Long> confirmUserInfoApi (@PathVariable String userName) {
-        return ApiResponseUtil.success(userService.confirmUserInfoApi(userName));
+    @GetMapping ("/confirm/{userId}")
+    public CommonResponse<Long> confirmUserInfoApi (@PathVariable Long userId) {
+        return ApiResponseUtil.success(userService.confirmUserInfoApi(userId));
     }
 
     @GetMapping ("/DeliveryAddressApi/{userId}")
