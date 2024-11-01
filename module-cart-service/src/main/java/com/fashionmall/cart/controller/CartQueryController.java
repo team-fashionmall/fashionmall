6cdd8bc639d6e0627f8e52d57f3dec588443e571
minnieming <eunmilee9109.1@gmail.com>
2024-11-02 00:48:37 +0900

    refactor : cart 유저 인증 코드

--- module-cart-service/src/main/java/com/fashionmall/cart/controller/CartQueryController.java
+++ module-cart-service/src/main/java/com/fashionmall/cart/controller/CartQueryController.java
@@ -3,8 +3,10 @@ package com.fashionmall.cart.controller;
 import com.fashionmall.cart.dto.response.CartResponseDto;
 import com.fashionmall.cart.service.CartService;
 import com.fashionmall.common.response.CommonResponse;
+import com.fashionmall.common.security.UserDetailsImpl;
 import com.fashionmall.common.util.ApiResponseUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,7 @@ public class CartQueryController {
     private final CartService cartService;
 
     @GetMapping("/cart")
-    public CommonResponse<List<CartResponseDto>> getCartList() {
-        Long userId = 1L;
-        return ApiResponseUtil.success(cartService.getCartList(userId));
+    public CommonResponse<List<CartResponseDto>> getCartList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ApiResponseUtil.success(cartService.getCartList(userDetails.getUserid()));
     }
 }
