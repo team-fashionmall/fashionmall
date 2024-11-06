ee94e22ba2203f5910d1d28752d9175a309b9116
minnieming <eunmilee9109.1@gmail.com>
2024-11-07 01:56:57 +0900

    feat : docker 오류

--- module-common/src/main/java/com/fashionmall/common/jwt/JwtUtil.java
+++ module-common/src/main/java/com/fashionmall/common/jwt/JwtUtil.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
