9a2e11b1ec6d83bc1201897a42e1fa5800b31113
noah <qkrwldn0818@naver.com>
2024-11-12 17:07:57 +0900

    eureka test

--- script/eureka/deploy.sh
+++ script/eureka/deploy.sh
@@ -4,8 +4,27 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
+IDLE_PROFILE=$(find_idle_profile)
 IDLE_PORT=$(find_idle_port)
-REPOSITORY=/home/ec2-user/app/demo/module-eureka-server # 서비스 모듈 경로(각 모듈 마다 다름)
+REPOSITORY=/home/ec2-user/app/demo/module-eureka-server
+
+CONTAINER_ID=$(docker container ls -f "name=eureka-${IDLE_PROFILE}" -q)
+
+echo "> 컨테이너 ID는 무엇?? ${CONTAINER_ID}"
+echo "> 현재 프로필은 무엇?? eureka-${IDLE_PROFILE}"
+
+if [ -z ${CONTAINER_ID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> docker stop eureka-${IDLE_PROFILE}"
+  sudo docker stop eureka-${IDLE_PROFILE}
+  echo "> docker rm eureka-${IDLE_PROFILE}"
+  sudo docker rm eureka-${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
+  sleep 5
+fi
+
+# 구분
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
@@ -22,8 +41,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
-IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=eureka-$IDLE_PROFILE 로 실행합니다."
 
