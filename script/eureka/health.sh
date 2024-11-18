a4677d1aaef924ece46a7fd1731cd3f46483a894
noah <qkrwldn0818@naver.com>
2024-11-18 15:37:11 +0900

    eureka 서버 지역 설정

--- script/eureka/health.sh
+++ script/eureka/health.sh
@@ -14,14 +14,15 @@ sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$(curl -s http://43.203.244.137:${IDLE_PORT})
-  UP_COUNT=$(echo ${RESPONSE} | grep 'eureka' | wc -l)
+  RESPONSE=$(curl -s -w "%{http_code}" -o /dev/null http://43.203.244.137:${IDLE_PORT})
 
-  if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("eureka" 문자열이 있는지 검증)
+if [ ${RESPONSE} -eq 404 ]
+  then # $up_count >= 1 ("gateway" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
       break
+  elif [ "$RESPONSE" -eq 000 ]; then
+      echo "> 연결 실패: 네트워크 문제나 서비스 다운 가능성"
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
       echo "> Health check: ${RESPONSE}"
