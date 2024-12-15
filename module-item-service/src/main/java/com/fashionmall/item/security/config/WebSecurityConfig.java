ba40a14679d294dcb8b36536541c674467519210
evan-song2 <mj.song@pet-friends.co.kr>
2024-12-15 22:17:33 +0900

    feat: cors 정책 설정 추가

--- module-item-service/src/main/java/com/fashionmall/item/security/config/WebSecurityConfig.java
+++ module-item-service/src/main/java/com/fashionmall/item/security/config/WebSecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Slf4j(topic = "webSecurityConfig")
 @Configuration
@@ -32,9 +35,24 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // 모든 Origin 허용
+        configuration.addAllowedMethod("*");       // 모든 HTTP Method 허용
+        configuration.addAllowedHeader("*");       // 모든 헤더 허용
+        configuration.setAllowCredentials(true);   // 인증 정보 포함 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http.formLogin(AbstractHttpConfigurer::disable);
         http.logout(AbstractHttpConfigurer::disable);
 
