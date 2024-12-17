2a6c192db563f0d28d2f0af56395708014e142dc
noah <qkrwldn0818@naver.com>
2024-12-17 19:13:55 +0900

    불필요한 컨테이너 이미지 삭제3

--- script/review/health.sh
+++ script/review/health.sh
@@ -6,6 +6,7 @@ source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -21,6 +22,21 @@ do
   then # $up_count >= 1 ("review" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
+      echo "> Nginx 프록시가 새 컨테이너로 전환되었습니다."
+
+            # 현재 사용 중인 프로필에 따라 이전 컨테이너 중단 및 삭제
+            if [ "${IDLE_PROFILE}" == "green" ]; then
+                echo "> 이전 컨테이너 중단 및 삭제 (review-blue)"
+                sudo docker stop review-blue
+                sudo docker rm review-blue
+                echo "> review-blue 컨테이너가 중단되고 삭제되었습니다."
+            else
+                echo "> 이전 컨테이너 중단 및 삭제 (review-green)"
+                sudo docker stop review-green
+                sudo docker rm review-green
+                echo "> review-green 컨테이너가 중단되고 삭제되었습니다."
+            fi
+
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
--- script/user/health.sh
+++ script/user/health.sh
@@ -6,6 +6,7 @@ source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -21,6 +22,21 @@ do
   then # $up_count >= 1 ("user" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
+      echo "> Nginx 프록시가 새 컨테이너로 전환되었습니다."
+
+            # 현재 사용 중인 프로필에 따라 이전 컨테이너 중단 및 삭제
+            if [ "${IDLE_PROFILE}" == "green" ]; then
+                echo "> 이전 컨테이너 중단 및 삭제 (user-blue)"
+                sudo docker stop user-blue
+                sudo docker rm user-blue
+                echo "> user-blue 컨테이너가 중단되고 삭제되었습니다."
+            else
+                echo "> 이전 컨테이너 중단 및 삭제 (user-green)"
+                sudo docker stop user-green
+                sudo docker rm user-green
+                echo "> user-green 컨테이너가 중단되고 삭제되었습니다."
+            fi
+
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
