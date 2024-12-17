3d50f572b685dc3e60693bb1715b1487003e69e3
noah <qkrwldn0818@naver.com>
2024-12-17 17:23:54 +0900

    원복

--- script/item/health.sh
+++ script/item/health.sh
@@ -21,25 +21,6 @@ do
   then # $up_count >= 1 ("item" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
-      # 기존 컨테이너 삭제
-      echo "> 기존 컨테이너 중지"
-      if [ "$RESPONSE" == "green" ]
-      then
-        sudo docker stop item-blue
-      else
-        sudo docker stop item-green
-      fi
-
-      # 필요시 컨테이너와 이미지 삭제
-      echo "> 기존 컨테이너 및 이미지 삭제"
-      if [ "$RESPONSE" == "green" ]
-      then
-        sudo docker rm item-blue
-      else
-        sudo docker rm item-green
-      fi
-
-      # 더 이상 재시도할 필요 없이 종료
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
