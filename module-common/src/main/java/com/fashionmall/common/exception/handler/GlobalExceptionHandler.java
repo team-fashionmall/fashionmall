bdfd9daee8d11ea036ad6f7198a131570a5c9129
noah <qkrwldn0818@naver.com>
2024-12-18 20:27:29 +0900

    feat: handleMethodArgumentNotValid 추가, common모듈 수정시 서비스 전체 재배포

--- module-common/src/main/java/com/fashionmall/common/exception/handler/GlobalExceptionHandler.java
+++ module-common/src/main/java/com/fashionmall/common/exception/handler/GlobalExceptionHandler.java
@@ -1,13 +1,15 @@
 package com.fashionmall.common.exception.handler;
 
 import com.fashionmall.common.exception.CustomException;
-import com.fashionmall.common.response.CommonResponse;
 import com.fashionmall.common.exception.ErrorResponseCode;
+import com.fashionmall.common.response.CommonResponse;
 import com.fashionmall.common.util.ApiResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -40,5 +42,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("Exception", exception);
         CommonResponse<Object> response = ApiResponseUtil.failure(statusCode.value(), exception.getMessage());
         return ResponseEntity.status(statusCode).body(response);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("ValidationException", ex);
+
+        FieldError fistFieldError = ex.getBindingResult().getFieldErrors().get(0);
+        CommonResponse<Object> response = ApiResponseUtil.failure(status.value(), fistFieldError.getField() + " : " + fistFieldError.getDefaultMessage());
+
+        return ResponseEntity.status(status).body(response);
     }
 }
